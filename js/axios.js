4dbc10a9ff104e2135ca6001c027a2acfaff181e
Gabi <gari.327@gmail.com>
2020-11-25 11:19:43 +0100

    mas

--- js/axios.js
+++ js/axios.js
@@ -3,3 +3,28 @@ const readRequest = document.querySelector('#readAll')
 const readRequest = document.querySelector('#read')
 const updateRequest = document.querySelector('#update')
 const deleteRequest = document.querySelector('#delete')
+
+postRequest.addEventListener('click', async(e)=>{
+    e.preventDefault()
+   const resultado = await axios.post('https://blog-express-gabi.herokuapp.com/api/blogs',{data: blogPrueba})
+})
+readAllRequest.addEventListener('click', async (e)=>{
+    e.preventDefault()
+    const resultado = await axios.get('https://blog-express-gabi.herokuapp.com/api/blogs')
+    console.log(resultado);
+})
+readRequest.addEventListener('click',(e)=>{
+    e.preventDefault()
+    const resultado = await axios.get('https://blog-express-gabi.herokuapp.com/api/blogs/5fbbaf3e72dc32001794327e')
+    console.log(resultado);
+})
+updateRequest.addEventListener('click',(e)=>{
+    e.preventDefault()
+    const resultado = await axios.patch('https://blog-express-gabi.herokuapp.com/api/blogs/5fbbaf3e72dc32001794327e',{data: {marca: 'Opela'}})
+    console.log(resultado);
+})
+deleteRequest.addEventListener('click',(e)=>{
+    e.preventDefault()
+    const resultado = await axios.delete('https://blog-express-gabi.herokuapp.com/api/blogs/5fbbaf3e72dc32001794327e')
+    console.log(resultado);
+})
